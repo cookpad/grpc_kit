4bac294a3b3f243b80a627c665f5ff72dc420030
ganmacs <ganmacs@gmail.com>
2018-10-06 12:35:15 +0900

    logger

--- lib/grpc_kit.rb
+++ lib/grpc_kit.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
+require 'logger'
+
 require 'grpc_kit/version'
 require 'grpc_kit/server'
 
 module GrpcKit
-  # Your code goes here...
+  def self.logger
+    @logger ||= Logger.new(STDOUT, level: :debug) # TODO: use :info level
+  end
+
+  def self.logger=(logger)
+    @logger = logger
+  end
 end
