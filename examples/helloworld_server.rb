ea8f21e09e8c6986e187d3e5820510ac9a2c8307
ganmacs <ganmacs@gmail.com>
2018-10-06 10:58:20 +0900

    Add examples

---
+++ examples/helloworld_server.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path('./examples/helloworld')
+
+require 'grpc_kit'
+require 'socket'
+require 'helloworld_services_pb'
+
+class GreeterServer < Helloworld::Greeter::Service
+  def say_hello(hello_req, _unused_call)
+    Helloworld::HelloReply.new(message: "Hello #{hello_req.name}")
+  end
+end
+
+sock = TCPServer.new(3000)
+
+server = GrpcKit::Server.new
+server.handle(GreeterServer)
+server.run
+
+loop do
+  conn = sock.accept
+  server.session_start(conn)
+end
