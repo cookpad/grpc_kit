43adfbb077a9b405d57fdfcd3e0937c17ffefb44
ganmacs <ganmacs@gmail.com>
2018-10-23 12:09:43 +0900

    Remove unused code

--- spec/grpc_kit_spec.rb
+++ spec/grpc_kit_spec.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 RSpec.describe GrpcKit do
-  it 'has a version number' do
-    expect(GrpcKit::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,8 @@ require 'bundler/setup'
 require 'grpc_kit'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
