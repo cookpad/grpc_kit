a485a31f3379b9594540d4170635ae37679a6b0f
ganmacs <ganmacs@gmail.com>
2018-10-24 19:32:50 +0900

    Add error rpc when rpc is not determined

--- lib/grpc_kit/rpcs.rb
+++ lib/grpc_kit/rpcs.rb
@@ -4,6 +4,7 @@ require 'grpc_kit/rpcs/request_response'
 require 'grpc_kit/rpcs/client_streamer'
 require 'grpc_kit/rpcs/server_streamer'
 require 'grpc_kit/rpcs/bidi_streamer'
+require 'grpc_kit/rpcs/error'
 
 module GrpcKit
   module Rpcs
---
+++ lib/grpc_kit/rpcs/error.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'grpc_kit/status_codes'
+
+module GrpcKit
+  module Rpcs
+    module Client
+      class Error
+        # def invoke(session, request, authority:, error)
+        # end
+      end
+    end
+
+    module Server
+      class Error
+        def send_bad_status(stream, session, bad_status)
+          ss = GrpcKit::Streams::Server.new(stream: stream, protobuf: nil, session: session)
+          ss.send_status(status: bad_status.code, msg: bad_status.message)
+        end
+      end
+    end
+  end
+end
