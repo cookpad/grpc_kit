64c36eff7488ce771d5fc26c794b19707f5d9b16
ganmacs <ganmacs@gmail.com>
2018-12-20 16:37:42 +0900

    call_stream call each by itself

--- examples/interceptors/call_stream.rb
+++ examples/interceptors/call_stream.rb
@@ -4,12 +4,17 @@ require 'grpc_kit'
 require 'forwardable'
 
 class CallStream < GrpcKit::Call
+  include Enumerable
   extend Forwardable
   delegate %i[send_msg recv] => :@inner
 
   # @params call [GrpcKit::Call]
   def initialize(inner)
     @inner = inner
+  end
+
+  def each
+    loop { yield(recv) }
   end
 
   def method_missing(name, *args, &block)
