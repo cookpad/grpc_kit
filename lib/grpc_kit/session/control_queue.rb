df219285f66f87ca60c15debce523e27cde9de9e
Sorah Fukumori <sorah@cookpad.com>
2020-09-28 12:42:03 +0900

    Session::IO#wake!: memo argument for debug purpose

--- lib/grpc_kit/session/control_queue.rb
+++ lib/grpc_kit/session/control_queue.rb
@@ -21,17 +21,17 @@ module GrpcKit
 
       def submit_response(id, headers)
         @event_stream.push([:submit_response, id, headers])
-        @waker.call
+        @waker.call(:submit_response)
       end
 
       def submit_headers(id, headers)
         @event_stream.push([:submit_headers, id, headers])
-        @waker.call
+        @waker.call(:submit_headers)
       end
 
       def resume_data(id)
         @event_stream.push([:resume_data, id])
-        @waker.call
+        @waker.call(:submit_response)
       end
     end
   end
